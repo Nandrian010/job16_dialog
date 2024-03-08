f9b93a56c5204cf1a92bb5e69001f010d671234b
PC-PPLG <adibmuzakki2021@gmail.com>
2024-03-08 10:00:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/job16/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.job16
+
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tombol = findViewById<Button>(R.id.tombolDialog)
+
+        tombol.setOnClickListener {
+            AlertDialog.Builder( this)
+                .setTitle("Judul")
+                .setMessage("Pesan yang tampil")
+                .setPositiveButton( "Yes", DialogInterface.OnClickListener {dialogInterface, i ->
+                    Toast.makeText(this,"Tombol Yes Ditekan", Toast.LENGTH_LONG).show()
+                })
+                .setNegativeButton("No", DialogInterface.OnClickListener{dialogInterface, i ->
+                    Toast.makeText(this,"Tombol No Ditekan", Toast.LENGTH_LONG).show()
+                })
+                .show()
+
+        }
+    }
+}
